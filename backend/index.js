555c4e44cf129f7e3e62e5780e5929b035815dce
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2023-01-04 12:13:39 +0530

    cors adjusted

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = 3001;
 const io = new Server(server, {
   cors: {
     allowedHeaders:"*",
-    origin: "http://localhost:3000",
+    origin: "https://illustrious-florentine-c6e22e.netlify.app",
     methods: ["GET", "POST"],
     credentials: true
   },
